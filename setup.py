e19c5862bd5a62ad09c707161d3109a0bfb825f6
Rob Speer <rob@luminoso.com>
2017-02-25 20:28:42 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import sys
+
+setup(
+    name="findvoices",
+    version='0.1',
+    maintainer='Rob Speer',
+    maintainer_email='rob.speer@gmail.com',
+    license="MIT",
+    url='http://github.com/rspeer/findvoices',
+    platforms=["any"],
+    description="Use the Twitter API for good",
+    packages=['findvoices'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
