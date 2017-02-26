730dab2518ba68eaf6b07dcee8311aa2bfb7527a
Rob Speer <rob@luminoso.com>
2017-02-25 22:06:55 -0500

    add certifi as a requirement

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['twitter', 'carmen']
+    install_requires=['twitter', 'carmen', 'certifi']
 )
